74f38df14f67e18f9c166d8d456859d77e9703e9
AiJiangnan <904629998@qq.com>
2018-11-01 20:05:56 +0800

    [add] add RedisCacheable.java comment

--- src/main/java/com/ajn/springboot/studyspringboot/annotation/RedisCacheable.java
+++ src/main/java/com/ajn/springboot/studyspringboot/annotation/RedisCacheable.java
@@ -7,8 +7,18 @@ import java.lang.annotation.*;
 @Documented
 public @interface RedisCacheable {
 
+    /**
+     * 缓存Redis时的Key
+     *
+     * @return Redis缓存的键
+     */
     String value();
 
+    /**
+     * 缓存过期时间，单位：毫秒
+     *
+     * @return 过期时间
+     */
     long timeout() default -1;
 
 }
